50f1c85b40ead647b4b99b9ffd9aea94aad4bd7c
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-12-21 18:30:50 +0200

    initial

---
+++ annotations/variables.ts
@@ -0,0 +1,16 @@
+let apples = 5;
+let userName: string = "Adham";
+let hasMobile: boolean = true;
+let nothing: null = null;
+let date: Date = new Date();
+let colors: string[] = ["red", "blue", "green"];
+class Player {}
+let playerOne = new Player();
+// playerOne = new Date();
+let point: {
+  x: number;
+  y: number;
+} = {
+  x: 3,
+  y: 4,
+};
